f7276b1f86dd04c6436b81c37ded51dd6b68360c
PablitoVerde <pgreeny@mail.ru>
2021-11-08 17:50:48 +0300

    Домашнее задание к 4 уроку.

---
+++ Lesson4/Task-1/Task-1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+/*
+ * Написать метод GetFullName(string firstName, string lastName, string
+patronymic), принимающий на вход ФИО в разных аргументах и возвращающий
+объединённую строку с ФИО. Используя метод, написать программу, выводящую в консоль
+3–4 разных ФИО.
+*/
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Приветствие и получение пользовательских данных
+            Console.WriteLine("Привет!");
+            Console.Write("Сколько пользователей будем выводить? ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            //цикл на перечисление всех людей
+            for (int i = 0; i < count; i++)
+            {
+                (string personLastname, string personFirstname, string personPatronymic) = GetUserData();
+                PrintFullName(GetFullName(personLastname, personFirstname, personPatronymic));
+            }   
+
+            Console.ReadKey();
+        }
+
+        //метод получения от пользователя ФИО в разных переменных
+        static (string, string, string) GetUserData()
+        {
+            Console.Write("Введите фамилию: ");
+            string _lastname = Console.ReadLine();
+
+            Console.Write("Введите имя: ");
+            string _firstname = Console.ReadLine();
+
+            Console.Write("Введите отчество: ");
+            string _patronymic = Console.ReadLine();
+
+            return (_lastname, _firstname, _patronymic);
+        }
+
+        //метод создания одной строки с ФИО
+        static string GetFullName(string lastname, string firstName, string patronymic)
+        {
+            return ($"{lastname} {firstName} {patronymic}.");
+        }
+
+        //метод вывода на экран ФИО
+        static void PrintFullName(string fullName)
+        {
+            Console.WriteLine(fullName);
+        }
+    }
+}
---
+++ Lesson4/Task-5/Task-5/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+/*
+ * Вернуть наибольшую совпадающую подстроку из двух строк.
+ */
+
+namespace Task_5
+{
+    class Program
+    {
+        //публичная переменная, которая хранит текущий ответ на задачу (можно решить ref/out, включив данную переменную, вероятно, в метод мэйн).
+        static string finalResult = "";
+        static void Main()
+        {
+            //Получение пользовательских данных
+            Console.WriteLine("Программа по поиску наибольшей подстроки среди двух строк.");
+
+            Console.WriteLine("Введите первую строку.");
+            string stringNumberOne = Console.ReadLine();
+
+            Console.WriteLine("Введите вторую строку.");
+            string stringNumberTwo = Console.ReadLine();
+
+            //Создание массивов символов
+            char[] chars1 = stringNumberOne.ToCharArray();
+            char[] chars2 = stringNumberTwo.ToCharArray();
+
+            string stringResult = "";          
+
+            //перечисление всех элементов двух массивов
+            for (int i = 0; i < chars1.Length; i++)
+            {
+                for (int j = 0; j < chars2.Length; j++)
+                {
+                    stringResult = GetLine(chars1, chars2, i, j, stringResult);
+                }
+            }
+
+            Console.WriteLine(finalResult);
+
+            Console.ReadKey();
+
+        }
+
+        //Метод, принимает 2 массива, текущий индекс в обоих массивах и промежуточный результат
+        static string GetLine(char[] _chars1, char[] _chars2, int _i, int _j, string _result)
+        {
+            if (_i != _chars1.Length && _j != _chars2.Length)
+            {
+                if (_chars1[_i] == _chars2[_j])
+                {
+                    _result += _chars1[_i];
+
+                    GetLine(_chars1, _chars2, _i + 1, _j + 1, _result);
+                }
+                
+                //Прекратить заполнять новую строку совпадающих переменных, если очередной элемент не совпал
+                else
+                {
+                    GetMax(_result);
+                    _result = "";
+                }
+            }
+            //Прекратить заполнять новую строку совпадающих переменных, если один из массивов закончился
+            else
+            {
+                GetMax(_result);
+                _result = "";
+            }
+
+            return _result;
+        }
+
+        //Метод сравнения двух строк решения: содержащую итоговый ответ и содержащую текущий ответ
+        static void GetMax(string _result)
+        { 
+            if (finalResult.Length < _result.Length)
+                finalResult = _result;          
+        }
+    }
+}
